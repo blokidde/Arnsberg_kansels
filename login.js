159789b00aee460f8992f331fea003b1a4005768
egmondj10 <jurriaan.egmond@hva.nl>
2025-08-19 14:08:56 +0200

    added new ngrok URL

--- login.js
+++ login.js
@@ -1,5 +1,5 @@
 // API base URL for authentication endpoints
-const API_URL = "https://461a-2001-1c08-883-4400-f0c3-e205-3254-d3c3.ngrok-free.app";
+const API_URL = "https://ec9e59218665.ngrok-free.app/";
 
 // Function to check if the stored token is valid
 function isTokenValid() {
